a1fc5ff1b81181451a33be6fa36e3ec052ff6abf
Christopher Davenport <Chris@ChristopherDavenport.tech>
2020-06-16 15:15:25 -0700

    Linking Error Class Added

--- core/src/test/scala/io/chrisdavenport/specs2jstest/MainSpec.scala
+++ core/src/test/scala/io/chrisdavenport/specs2jstest/MainSpec.scala
@@ -1,10 +1,12 @@
 package io.chrisdavenport.specs2jstest
 
 import org.specs2._
+import org.specs2.matcher.FutureMatchers
 import org.specs2.concurrent.ExecutionEnv
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
-class SeemsToWorkSpec extends mutable.Specification {
+class SeemsToWorkSpec extends mutable.Specification with FutureMatchers {
   import scala.concurrent.ExecutionContext.Implicits.global
 
   "Seems to work with the global Execution Context" should {
@@ -22,3 +24,13 @@ class FailsToInitialize(implicit ee: ExecutionEnv) extends mutable.Specification
     }
   }
 }
+
+class LinkingErrorClass(implicit ee: ExecutionEnv) extends mutable.Specification {
+  "Fails on await" in {
+    futureAsResult(Future(ok)).awaitFor(1.second)
+  }
+}
+
+// What I want is given an IO[Result]. Have some way to evaluate that with specs2 on scalajs.
+// Any ideas to that end appreciated.
+// https://github.com/djspiewak/cats-effect-testing/pull/70/files
